0abd765cae2665614beaf9ee46e5854999713502
Grant Mathews <grant.mathews@sqor.com>
2015-03-08 16:11:06 -0700

    Add bridging header with font awesome

---
+++ BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  FontAwesome.h
+//  
+//
+//  Created by Grant Mathews on 3/8/15.
+//
+//
+
+#ifndef _FontAwesome_h
+#define _FontAwesome_h
+
+#import "FontAwesomeKit/FontAwesomeKit.h"
+
+#endif
