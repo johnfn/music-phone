0930458f79da723cf637b376b3b3b3eb6fea98c8
Nam Kim <namxkim@gmail.com>
2015-03-08 15:39:45 -0700

    Add parse.

--- MusicPhone/AppDelegate.swift
+++ MusicPhone/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.enableLocalDatastore()
+        Parse.setApplicationId("BI5Loygir2bKJ8FPKdfKBzaBBhxgHc3Tl9ahB3QW", clientKey: "vGCI0Atg4yqLXFh4awokGd9vDFCtnMNYSJx4zL1F")
+        
         return true
     }
 
