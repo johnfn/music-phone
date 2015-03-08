4380502c4e575cc5f219ea1e5b08e20de30d0090
Nam Kim <namxkim@gmail.com>
2015-03-08 15:51:01 -0700

    Add sound player class.

---
+++ MusicPhone/SoundPlayer.swift
@@ -0,0 +1,13 @@
+//
+//  SoundPlayer.swift
+//  MusicPhone
+//
+//  Created by Michael Kim on 3/8/15.
+//  Copyright (c) 2015 Grant Mathews. All rights reserved.
+//
+
+import Foundation
+
+class SoundPlayer {
+    
+}
