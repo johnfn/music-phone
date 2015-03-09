f64534a4a1e1669a330fd28942b7196b2c904bde
Nam Kim <namxkim@gmail.com>
2015-03-08 17:02:25 -0700

    Add outlets for registration view controller.

--- MusicPhone/RegistrationViewController.swift
+++ MusicPhone/RegistrationViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class RegistrationViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
 
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
